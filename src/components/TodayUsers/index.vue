313f7a48928a52347f85b29d09368e02715be2e8
tsai <mengjietsai@163.com>
2020-07-11 14:06:22 +0800

    累计订单量图表的实现

--- src/components/TodayUsers/index.vue
+++ src/components/TodayUsers/index.vue
@@ -1,14 +1,21 @@
 <template>
-  <common-card/>
+  <common-card
+    title="今日交易用户数"
+    value=" 39,165">
+    <template v-slot:footer>
+      <div>
+        <span>退货率 </span>
+        <span class="emphasis">5.14%</span>
+      </div>
+    </template>
+  </common-card>
 </template>
 
 <script>
-import CommonCard from '../CommonCard'
+import commonCardMixin from '../../mixins/commonCardMixin'
 
 export default {
-  components: {
-    CommonCard
-  }
+  mixins: [commonCardMixin]
 }
 </script>
 
--- src/components/TotalUsers/index.vue
+++ src/components/TotalUsers/index.vue
@@ -1,17 +1,34 @@
 <template>
-  <common-card/>
+  <common-card title="今日交易用户数"
+               value=" 39,165">
+    <template v-slot:footer>
+      <div class="compare-wrapper">
+        <div class="compare">
+          <span>日同比</span>
+          <span class="emphasis">7.33%</span>
+          <div class="increase"></div>
+        </div>
+        <div class="compare">
+          <span>月同比</span>
+          <span class="emphasis">17.33%</span>
+          <div class="decrease"></div>
+        </div>
+      </div>
+    </template>
+  </common-card>
 </template>
 
 <script>
-import CommonCard from '../CommonCard'
+import commonCardMixin from '../../mixins/commonCardMixin'
 
 export default {
-  components: {
-    CommonCard
-  }
+  mixins: [commonCardMixin]
 }
 </script>
 
-<style scoped>
+<style lang="scss" scoped>
 
+  .compare-wrapper {
+    display: flex;
+  }
 </style>
