d2e5714f6203627d83eae3da796d3bdc01b5a720
tsai <mengjietsai@163.com>
2020-07-11 10:01:57 +0800

    element-ui echarts 安装 容器组件初始化

--- src/App.vue
+++ src/App.vue
@@ -1,11 +1,17 @@
 <template>
   <div id="app">
-    hhh
-    <router-view />
+    <router-view/>
   </div>
 </template>
 
-<style lang="scss">
+<script>
+
+export default {
+  name: 'app'
+}
+</script>
+
+<style>
   html, body, #app {
     width: 100%;
     height: 100%;
