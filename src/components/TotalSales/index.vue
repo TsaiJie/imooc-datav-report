e32405839e063c3ab26ef333ed85f9b0532a7b6a
tsai <mengjietsai@163.com>
2020-07-11 10:44:25 +0800

    CommonCard组件的基本样式

--- src/components/TotalSales/index.vue
+++ src/components/TotalSales/index.vue
@@ -1,5 +1,18 @@
 <template>
-  <common-card/>
+  <common-card
+    title="累计销售额"
+    value="¥ 32,039,165"
+  >
+    <template >
+      <div class="compare"></div>
+    </template>
+    <template v-slot:footer>
+      <div>
+        <span>昨日销售额</span>
+        <span class="money">¥ 30,000,000</span>
+      </div>
+    </template>
+  </common-card>
 </template>
 
 <script>
@@ -10,6 +23,17 @@ export default {
 }
 </script>
 
-<style scoped>
-
+<style lang="scss" scoped>
+  span {
+    font-size: 12px;
+  }
+  .money {
+    margin-left: 5px;
+    color: #333;
+    font-weight: 700;
+  }
+  .compare {
+    height: 100%;
+    background: yellowgreen;
+  }
 </style>
