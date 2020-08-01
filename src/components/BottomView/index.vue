b2144bc8d91ad25ea747ddc13599c386e47b6973
tsai <mengjietsai@163.com>
2020-08-01 15:14:22 +0800

    关键词搜索部分完成

--- src/components/BottomView/index.vue
+++ src/components/BottomView/index.vue
@@ -7,23 +7,36 @@
         </template>
         <template>
           <div class="chart-wrapper">
-            <div class="chart">
-              <div class="chart-title">搜索用户数</div>
-              <div class="chart-data">93,635</div>
-              <v-chart :options="searchUserOptions"/>
+            <div class="chart-inner">
+              <div class="chart">
+                <div class="chart-title">搜索用户数</div>
+                <div class="chart-data">93,635</div>
+                <v-chart :options="searchUserOptions"/>
+              </div>
+              <div class="chart">
+                <div class="chart-title">搜索量</div>
+                <div class="chart-data">193,635</div>
+                <v-chart :options="searchUserOptions"/>
+              </div>
             </div>
-            <div class="chart">
-              <div class="chart-title">搜索量</div>
-              <div class="chart-data">193,635</div>
-              <v-chart :options="searchNumOptions"/>
+            <div class="table-wrapper">
+              <el-table :data="tableData">
+                <el-table-column prop="rank" label="排名" width="180"/>
+                <el-table-column prop="keyword" label="关键字" width="180"/>
+                <el-table-column prop="count" label="搜索总量"/>
+                <el-table-column prop="users" label="搜索用户数"/>
+              </el-table>
+              <el-pagination
+                layout="prev, pager, next"
+                :total="100"
+                :page-size="4"
+                background
+                @current-change="onPageChange"
+
+              />
             </div>
           </div>
-          <div class="table-wrapper">
-            <el-table :data="tableData">
-              <el-table-column/>
-            </el-table>
-            <el-pagination/>
-          </div>
+
         </template>
       </el-card>
     </div>
@@ -32,7 +45,7 @@
         <template v-slot:header>
           <div class="title-wrapper">
             <div class="title">分类销售排行</div>
-            <div class="radio-wrapper" >
+            <div class="radio-wrapper">
               <el-radio-group v-model="radioSelect" size="small">
                 <el-radio-button label="品类"></el-radio-button>
                 <el-radio-button label="商品"></el-radio-button>
@@ -41,7 +54,9 @@
           </div>
         </template>
         <template>
-          <v-chart :options="categoryOptions" />
+          <div class="chart-wrapper">
+            <v-chart :options="categoryOptions"/>
+          </div>
         </template>
       </el-card>
     </div>
@@ -52,11 +67,85 @@
 export default {
   data () {
     return {
-      searchUserOptions: {},
+      searchUserOptions: {
+        xAxis: {
+          type: 'category',
+          boundaryGap: false
+        },
+        yAxis: {
+          show: false
+        },
+        series: [{
+          type: 'line',
+          data: [100, 150, 200, 250, 200, 150, 100, 50, 100, 120],
+          areaStyle: {
+            color: 'rgba(95, 187, 255, .5)'
+          },
+          lineStyle: {
+            color: 'rgb(95, 187, 255)'
+          },
+          itemStyle: {
+            opacity: 0
+          },
+          smooth: true
+        }],
+        grid: {
+          top: 0,
+          left: 0,
+          right: 0,
+          bottom: 0
+        }
+      },
       searchNumOptions: {},
       radioSelect: '品类',
       categoryOptions: {},
-      tableData: []
+      tableData: [
+        {
+          id: 1,
+          rank: 1,
+          keyword: '北京',
+          count: 100,
+          users: 90,
+          range: '90%'
+        },
+        {
+          id: 2,
+          rank: 2,
+          keyword: '北京',
+          count: 100,
+          users: 90,
+          range: '90%'
+        },
+        {
+          id: 3,
+          rank: 3,
+          keyword: '北京',
+          count: 100,
+          users: 90,
+          range: '90%'
+        },
+        {
+          id: 4,
+          rank: 4,
+          keyword: '北京',
+          count: 100,
+          users: 90,
+          range: '90%'
+        },
+        {
+          id: 5,
+          rank: 5,
+          keyword: '北京',
+          count: 100,
+          users: 90,
+          range: '90%'
+        }
+      ]
+    }
+  },
+  methods: {
+    onPageChange (page) {
+      console.log(page)
     }
   }
 }
@@ -66,16 +155,20 @@ export default {
 .bottom-view {
   display: flex;
   margin-top: 20px;
+
   .view {
     flex: 1;
     width: 50%;
     box-sizing: border-box;
+
     &:first-child {
       padding: 0 10px 0 0;
     }
+
     &:last-child {
       padding: 0 0 0 10px;
     }
+
     .title-wrapper {
       display: flex;
       align-items: center;
@@ -85,11 +178,57 @@ export default {
       font-size: 14px;
       font-weight: 500;
       padding: 0 0 0 20px;
+
       .radio-wrapper {
         flex: 1;
         display: flex;
         justify-content: flex-end;
         padding-right: 20px;
+      }
+    }
+
+    .chart-wrapper {
+      display: flex;
+      flex-direction: column;
+      height: 452px;
+
+      .chart-inner {
+        display: flex;
+        padding: 0 10px;
+        margin-top: 20px;
+
+        .chart {
+          flex: 1;
+          padding: 0 10px;
+
+          .chart-title {
+            color: #999;
+            font-size: 14px;
+          }
+
+          .chart-data {
+            font-size: 22px;
+            color: #333;
+            font-weight: 500;
+            letter-spacing: 2px;
+          }
+
+          .echarts {
+            height: 50px;
+          }
+        }
+      }
+    }
+
+    .table-wrapper {
+      flex: 1;
+      margin-top: 20px;
+      padding: 0 20px 20px;
+
+      .el-pagination {
+        display: flex;
+        justify-content: flex-end;
+        margin-top: 15px;
       }
     }
   }
